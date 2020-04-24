107df3f813e79ac828378a36dbf5d6c4ee35f1c2
ahtchow <54782402+ahtchow@users.noreply.github.com>
2020-04-24 14:39:03 -0400

    Code Refactor: V1.0.1
    -> Made Initialization of Subscribers, Publishers, etc through new functions
    -> Comment, Formating, etc

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,19 @@
+import os
+import cv2
+import numpy as np 
+import rospy
+import tensorflow as tf
+
 from styx_msgs.msg import TrafficLight
 
+
 class TLClassifier(object):
-    def __init__(self):
-        #TODO load classifier
-        pass
+
+    def __init__(self, model_path):
+        # Initialize TLClassifier Class
+
+        self.current_light = TrafficLight.UNKNOWN
+
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
